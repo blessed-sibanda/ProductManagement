78a4897be429c6193ff7b851799798100b4d1745
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-13 12:27:27 +0200

    implement recommendation-service api

--- microservices/recommendation-service/src/main/java/me/blessedsibanda/microservices/core/recommendation/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/me/blessedsibanda/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -2,12 +2,14 @@ package me.blessedsibanda.microservices.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("me.blessedsibanda")
 public class RecommendationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecommendationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationServiceApplication.class, args);
+    }
 
 }
