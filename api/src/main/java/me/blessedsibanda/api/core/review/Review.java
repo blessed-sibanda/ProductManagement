9c1d078a7fe2d9bdbe452cc9227ce45165aac460
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-17 19:56:05 +0200

    add mysql persistence to review-service

--- api/src/main/java/me/blessedsibanda/api/core/review/Review.java
+++ api/src/main/java/me/blessedsibanda/api/core/review/Review.java
@@ -4,10 +4,12 @@ package me.blessedsibanda.api.core.review;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 public class Review {
     private int productId;
     private int reviewId;
--- microservices/review-service/src/test/java/me/blessedsibanda/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/me/blessedsibanda/microservices/core/review/ReviewServiceApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ReviewServiceApplicationTests {
+class ReviewServiceApplicationTests extends MySqlTestBase {
 	@Autowired
 	private WebTestClient client;
 
